d62d89333093336d519b1bf06ff488c74998906c
Baptiste <baptiste.lejeune@hotmail.com>
2022-10-14 20:54:34 +0200

    modif page confirmation envoi

--- app/Views/front/confirmContact.php
+++ app/Views/front/confirmContact.php
@@ -2,11 +2,13 @@
 require_once 'layouts/header.php';
 ?>
 
+<main class="container" id="confirmation">
 <h2>Votre demande a bien été envoyée !</h2>
 
 <p>Vous pouvez retourner à l'accueil en cliquant sur le bouton ci-dessous</p>
 
-<a href="index.php">Retourner à l'accueil</a>
+<a href="index.php" class="bouton-orange">Retourner à l'accueil</a>
+</main>
 
 
 <?php 
